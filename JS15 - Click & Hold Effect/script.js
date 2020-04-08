7a63aad8b2f9a12bfa4a66c478574dc40a1ee07d
Patiphan Phengpao <misterpatipan123@gmail.com>
2020-04-08 21:43:26 +0700

    Add files via upload

---
+++ JS15 - Click & Hold Effect/script.js
@@ -0,0 +1,10 @@
+let cursor = document.getElementById('cursor');
+
+window.onmousemove = function(e) {
+    let x = e.clientX,
+        y = e.clientY;
+    
+    cursor.style.left = x + 'px';
+    cursor.style.top = y + 'px';
+    console.log(x, y);
+}
